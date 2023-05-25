7dafc929f9b437d2b44409e80fbf514b14f46a4d
Richard <richard@tinyrock.com>
2023-05-25 15:10:30 +1000

    File with imports needed for notebook

---
+++ examples/example_imports.py
@@ -0,0 +1,11 @@
+import sys
+
+sys.path.append('..')
+
+from ruv.relative_utility_value import *
+from ruv.damage_functions import *
+from ruv.economic_models import *
+from ruv.utility_functions import *
+
+import numpy as np
+from matplotlib import pyplot as plt
