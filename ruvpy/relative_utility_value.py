3abe373ad5b6e19a5d2185cb2c3f4be8becd9052
Richard <richard@tinyrock.com>
2024-10-16 13:45:48 +1100

    Reformated docstring into google style

--- ruvpy/relative_utility_value.py
+++ ruvpy/relative_utility_value.py
@@ -21,110 +21,94 @@ from ruvpy.data_classes import DecisionContext
 # main entry function for RUV calculation
 def relative_utility_value(obs: np.ndarray, fcsts: np.ndarray, refs: np.ndarray, decision_context: dict, parallel_nodes: int=4) -> dict:
     """
-    Calculate the Relative Utility Value (RUV) for a set of observations, forecasts, and references using
-    specified decision-context.
+    Calculate the Relative Utility Value (RUV) for a set of observations, forecasts, and references
+    using a specified decision-context.
 
-    This entry function to RUVPY calculates RUV by evaluating the utility of forecasts through a simulation of
-    decision-making under uncertainty. RUV quantifies the value of forecasts relative to a reference scenario
-    (e.g., climatology) and is often applied in settings such as streamflow forecasting or weather forecasting,
-    where decisions are made based on forecast probabilities. It is typically used with probabilistic forecasts but
-    can also handle deterministic forecasts (single ensemble member for fcsts and refs).
+    This function calculates RUV by evaluating the utility of forecasts through a simulation of decision-making
+    under uncertainty. RUV quantifies the value of forecasts relative to a reference scenario (e.g., climatology)
+    and is often applied in settings such as streamflow forecasting or weather forecasting. It works with
+    probabilistic forecasts but can also handle deterministic forecasts (single ensemble member for `fcsts` and `refs`).
 
-    The RUV method and RUVPY software package are introduced in detail in the following publications. We suggest
-    reading these to understand the context and motivation for the software.
+    The RUV method and RUVPY software package are introduced in the following publications:
 
-    *Laugesen, R., Thyer, M., McInerney, D., and Kavetski, D.: Flexible forecast value metric suitable for a wide range
-    of decisions: application using probabilistic subseasonal streamflow forecasts, Hydrol. Earth Syst. Sci., 27,
-    873–893, https://doi.org/10.5194/hess-27-873-2023, 2023.*
+    - Laugesen, R., Thyer, M., McInerney, D., and Kavetski, D.: Flexible forecast value metric suitable for a wide range
+      of decisions: application using probabilistic subseasonal streamflow forecasts, Hydrol. Earth Syst. Sci., 27,
+      873–893, https://doi.org/10.5194/hess-27-873-2023, 2023.
+    - Laugesen, R., Thyer, M., McInerney, D., Kavetski, D. (2024). Software to quantify the value of forecasts for
+      decision-making: sensitivity to damages case study. Manuscript submitted for publication in Environmental
+      Modelling & Software.
 
-    *Laugesen, R., Thyer, M., McInerney, D., Kavetski, D. (2024). Software to quantify the value of forecasts for
-    decision-making: sensitivity to damages case study. Manuscript submitted for publication in Environmental
-    Modelling & Software.*
+    Args:
+        obs (np.ndarray): 1D array of observed values representing the actual outcomes.
+        fcsts (np.ndarray): 2D array of forecast values, where each column is an ensemble member and each row
+            corresponds to a forecast at a given time.
+        refs (np.ndarray): 2D array of reference values (e.g., climatology ensemble), where each column is an ensemble
+            member and each row corresponds to a time period. If `None`, a reference climatology will be generated based
+            on the observations, reproducing the observed frequency of events.
+        decision_context (dict): Dictionary defining the decision context, containing:
+            - 'decision_thresholds' (np.ndarray): 1D array specifying thresholds of the forecast variable.
+            - 'decision_rule' (list): Decision-making function and a dictionary of its parameters.
+            - 'damage_function' (list): Damage function method and a dictionary of its parameters.
+            - 'utility_function' (list): Utility function method and a dictionary of its parameters.
+            - 'economic_model' (list): Economic model function, analytical function, and list of parameter values.
+        parallel_nodes (int, optional): Number of parallel processes used for computation. Defaults to 4.
 
-    Parameters
-    ----------
-    obs : np.ndarray
-        1D array of observed values, representing the actual outcomes.
-    fcsts : np.ndarray
-        2D array of forecast values, where each column is an ensemble member and each row corresponds to a forecast at
-        a given time.
-    refs : np.ndarray
-        2D array of reference values (e.g., climatology ensemble), where each column is an ensemble member and each row
-        corresponds to a time period. If None, a reference climatology will be generated based on the observations,
-        reproduces the observed frequency of events as in REV.
-    decision_context : dict
-        Dictionary defining the decision-context. The dictionary must contain:
-        - 'decision_thresholds': 1D array specifying thresholds of the forecast variable.
-        - 'decision_rule': List with the decision-making function and a dictionary of its parameters.
-        - 'damage_function': List with the damage function method and a dictionary of its parameters.
-        - 'utility_function': List with the utility function method and a dictionary of its parameters.
-        - 'economic_model': List with the economic model function, analytical function, and list of parameter values.
-    parallel_nodes : int, optional
-        The number of parallel processes used for computation. Defaults to 4.
+    Returns:
+        dict: Dictionary containing the calculated Relative Utility Value (RUV) results. Keys include:
+            - 'ruv': 2D array of RUV values for each economic model parameter.
+            - 'economic_model_params': Economic model parameters used in the calculation.
+            - 'fcst_spends', 'ref_spends', 'obs_spends': Amount spent after decision optimization at each timestep.
+            - 'fcst_ex_ante', 'ref_ex_ante', 'obs_ex_ante': Expected utility before the event occurred (ex ante).
+            - 'fcst_ex_post', 'ref_ex_post', 'obs_ex_post': Utility after the event occurred (ex post).
+            - 'avg_fcst_ex_post', 'avg_ref_ex_post', 'avg_obs_ex_post': Average ex post utility.
 
-    Returns
-    -------
-    dict
-        Dictionary containing the calculated Relative Utility Value (RUV) results for the given forecasts.
-        The dictionary includes:
-        - 'ruv': A 2D array of RUV values for each economic model parameter.
-        - 'economic_model_params': The economic model parameters used in the calculation for convenience.
-        - 'fcst_spends', 'ref_spends', 'obs_spends': Amount spent after decision optimisation at each timestep.
-        - 'fcst_ex_ante', 'ref_ex_ante', 'obs_ex_ante': Expected utility before event occurred (ex ante).
-        - 'fcst_ex_post', 'ref_ex_post', 'obs_ex_post': Utility after event occurred (ex post).
-        - 'avg_fcst_ex_post', 'avg_ref_ex_post', 'avg_obs_ex_post': Average ex post utility.
+    Raises:
+        ValueError: If inputs contain missing values, invalid thresholds are provided, or input data lengths do not match.
 
-    Raises
-    ------
-    ValueError
-        If inputs contain missing values, invalid thresholds provided, or input data lengths do not match.
+    Examples:
+        Examples reproducing figures from the research papers can be found as Jupyter notebooks in the *examples* directory.
 
-    Examples
-    --------
-    Example decision context:
+        An example decision context may help to understand the input structure required.
 
-        decision_context = {
-            'utility_function': [cara, {'A': 0.3}],
-            'decision_rule': [optimise_over_forecast_distribution, None],
-            'decision_thresholds': np.array([0, 5, 15, 25]),
-            'economic_model': [cost_loss, cost_loss_analytical_spend, np.array([0.1, 0.5, 0.9])],
-            'damage_function': [logistic, {'k': 0.2, 'A': 1, 'threshold': 20}]
-        }
+            decision_context = {
+                'utility_function': [cara, {'A': 0.3}],
+                'decision_rule': [optimise_over_forecast_distribution, None],
+                'decision_thresholds': np.array([0, 5, 15, 25]),
+                'economic_model': [cost_loss, cost_loss_analytical_spend, np.array([0.1, 0.5, 0.9])],
+                'damage_function': [logistic, {'k': 0.2, 'A': 1, 'threshold': 20}]
+            }
 
-    Examples reproducing figures from related research are included as Jupyter notebooks in the *examples* directory.
+    Included decision context functions:
 
-    Included decision context functions
-    ----------------------------------------
+        Decision rules:
+            - `optimise_over_forecast_distribution`: Optimises decision-making based on the whole forecast distribution.
+            - `critical_probability_threshold_fixed`: Uses a fixed critical probability threshold for decision-making.
+            - `critical_probability_threshold_max_value`: Selects the decision threshold leading to the maximum forecast value.
+            - `critical_probability_threshold_equals_par`: Matches the decision threshold to the economic parameter.
 
-    **Decision rules**:
-    - `optimise_over_forecast_distribution`: Optimises decision-making based on whole forecast distribution.
-    - `critical_probability_threshold_fixed`: Uses a fixed critical probability threshold for decision-making.
-    - `critical_probability_threshold_max_value`: Selects the decision threshold leading to the maximum forecast value.
-    - `critical_probability_threshold_equals_par`: Matches the decision threshold to the economic parameter.
+        Damage functions:
+            - `logistic`: Logistic damage function with defined maximum damages, steepness, and location.
+            - `logistic_zero`: Logistic function with damages pegged to zero for zero flow.
+            - `binary`: Binary loss function with parameters for max and min loss, and location.
+            - `linear`: Linear damage function.
+            - `user_defined`: Damage function interpolated over user-defined points.
 
-    **Damage functions**:
-    - `logistic`: A logistic damage function with a defined maximium damages, steepness, and location.
-    - `logistic_zero`: A logistic function with damages pegged to zero for zero flow.
-    - `binary`: A binary loss function with parameters for max and min loss, and location.
-    - `linear`: A linear damage function.
-    - `user_defined`: A damage function interpolated over user-defined points.
+        Utility functions:
+            - `cara`: Constant Absolute Risk Aversion (CARA), where absolute risk aversion stays constant regardless of wealth.
+            - `crra`: Constant Relative Risk Aversion (CRRA), where relative risk aversion stays constant regardless of wealth.
+            - `exponential_utility`: Exponential utility function used to model CARA behaviour.
+            - `isoelastic_utility`: Isoelastic utility function used to model CRRA behaviour.
+            - `hyperbolic_utility`: Hyperbolic Absolute Risk Aversion (HARA), generalises both CARA and CRRA behavior.
 
-    **Utility functions**:
-    - `cara`: Constant Absolute Risk Aversion (CARA), where absolute risk aversion stays constant regardless of wealth.
-    - `crra`: Constant Relative Risk Aversion (CRRA), where relative risk aversion stays constant regardless of wealth.
-    - `exponential_utility`: Exponential utility function used to model CARA behaviour.
-    - `isoelastic_utility`: Isoelastic utility function used to model CRRA behaviour.
-    - `hyperbolic_utility`: Hyperbolic Absolute Risk Aversion (HARA), generalises both CARA and CRRA behaviour.
+        Economic models:
+            - `cost_loss`: Standard cost-loss economic model based on spending to mitigate potential future losses.
+            - `cost_loss_analytical_spend`: Analytical function to compute optimal spending in cost-loss.
 
-    **Economic models**:
-    - `cost_loss`: Standard cost-loss economic model based on spending to mitigate potential future losses.
-    - `cost_loss_analytical_spend`: An analytical function to compute optimal spending in cost-loss.
-
-    **Decision types**:
-    Defined by providing a list of thresholds in the `decision_thresholds` key of the decision_context dictionary
-    - Binary decision: 1D array with two elements, 0 and the threshold value (e.g., np.array([0, 20]))
-    - Multi-categorical decision: 1D array with multiple elements, one must be 0 (e.g., np.array([0, 5, 15, 25]))
-    - Continuous decision: None
+        Decision types:
+            Defined by providing a list of thresholds in the `decision_thresholds` key of the decision_context dictionary:
+            - Binary decision: 1D array with two elements, 0 and the threshold value (e.g., np.array([0, 20])).
+            - Multi-categorical decision: 1D array with multiple elements, one of which must be 0 (e.g., np.array([0, 5, 15, 25])).
+            - Continuous decision: `None`.
     """
 
     # decision type
